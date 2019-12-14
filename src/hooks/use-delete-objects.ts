e409414bc31b103494d89244ff31335882674169
Salvo <me@salvo.dev>
2019-12-14 15:46:44 +0000

    initial useDeleteObjects hook draft

---
+++ src/hooks/use-delete-objects.ts
@@ -0,0 +1,64 @@
+import { AWSError, S3 } from 'aws-sdk';
+import deleteObjectsReducer, { getInitialState } from '../reducers/delete-objects';
+import { useContext, useEffect, useReducer } from 'react';
+
+import { Client } from '../client';
+import ClientContext from '../contexts/client';
+
+const initialState = getInitialState();
+
+const useDeleteObjects = () => {
+	const [state, dispatch] = useReducer(deleteObjectsReducer, initialState);
+	const ctxClient = useContext<Client>(ClientContext);
+
+	// const loadFromKey = useCallback(() => {
+	// 	dispatch({ type: 'init' });
+	// }, [path]);
+
+	// load => keys[]
+	// run
+	// next - batch
+	// batch - completed
+	// error
+	// done
+
+	const addKeys = (keys: string[]) => {
+		dispatch({ type: 'load', keys });
+	};
+
+	const run = () => dispatch({ type: 'run' });
+
+	useEffect(() => {
+		if (state.pending.length && !state.next.length)
+			dispatch({ type: 'next-batch'});
+		else
+			dispatch({ type: 'done' });
+	}, [state.pending]);
+
+
+	// Sets the next file when it detects that its ready to go
+	useEffect(() => {
+		if (state.next.length) {
+			ctxClient.deleteObjects({
+				Delete: {
+					Objects: state.next.map(Key => ({ Key })),
+				},
+			})
+				.then((value: S3.DeleteObjectsOutput) => {
+					console.log('Deleted: ', value.Deleted);
+
+					dispatch({ type: 'batch-completed' });
+			}).catch((error: AWSError) => {
+				dispatch({ type: 'error', error });
+			});
+		}
+	}, [state.next]);
+
+	return {
+		state,
+		addKeys,
+		run,
+	}
+}
+
+export default useDeleteObjects;
