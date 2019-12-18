535a5df7d8a1f170782136c6fb70e656e9623b44
Salvo <me@salvo.dev>
2019-12-18 23:41:48 +0000

    make delete reducer

---
+++ src/reducers/delete-objects.ts
@@ -0,0 +1,58 @@
+interface DeleteObjectsState {
+	keys: string[];
+	pending: string[];
+	error: any;
+}
+
+export const getInitialState: () => DeleteObjectsState = () => ({
+	keys: [],
+	pending: [],
+	error: void 0,
+});
+
+export type DeleteObjectsAction =
+	| { type: 'load', keys: string[] }
+	| { type: 'run' }
+	| { type: 'completed' }
+	| { type: 'error', error: any }
+	;
+
+// todo: Note: filesState continuous deeply nested spreading is not great
+
+const deleteObjectsReducer = (state: DeleteObjectsState, action: DeleteObjectsAction): DeleteObjectsState => {
+	switch (action.type) {
+		case 'load':
+			return {
+				...state,
+				keys: action.keys,
+				pending: [],
+			};
+
+		// V simplified for now.
+		case 'run':
+			return {
+				...state,
+				keys: [],
+				pending: state.keys,
+			};
+
+		case 'completed':
+			return {
+				...state,
+				keys: [],
+				pending: [],
+			};
+
+		case 'error':
+			return {
+				keys: [],
+				pending: [],
+				error: action.error,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default putObjectsReducer;
